8fa4fa2498c4beea38102d9dc16caad00f3fda24
Hugo Dellinger <hugo.dellinger@gmail.com>
2020-05-03 14:53:24 +0200

    Display text and save status

--- force-app/main/default/lwc/quickNote/quickNote.js
+++ force-app/main/default/lwc/quickNote/quickNote.js
@@ -9,18 +9,59 @@ import retrieveQuickContentDocumentIdForRecordId from "@salesforce/apex/QuickNot
 export default class QuickNote extends LightningElement {
   @api recordId;
   @track quickContentDocumentVersionId;
+  noteRecord;
+  noteRecordContent; // To prevent decoding the content everytime we want to check equality with current content, we store the decoded string
+  htmlContent;
+
+  // Splitting the loading state for data fecthing in two steps
+  // enables us to stop displaying the spinner if no existing note has been found
+  // and thus no subsequent data loading happen
+  initialLoading = true;
+  dataLoading = false;
+  get isLoading() {
+    return this.initialLoading === true || this.dataLoading === true;
+  }
+
+  // We store the current content at each change to be able to show the correct state for the save button
+  get isContentSameAsBefore() {
+    return this.noteRecordContent == this.htmlContent; // eslint-disable-line eqeqeq
+  }
+  handleContentChange({ target }) {
+    this.htmlContent = target.value;
+  }
+
+  handleErrors({ error }) {
+    console.error(error);
+  }
 
   @wire(retrieveQuickContentDocumentIdForRecordId, { recordId: "$recordId" })
   wiredContentDocumentId({ error, data }) {
-    if (error) console.error(error); // TODO: error handling
-    if (data) this.quickContentDocumentVersionId = data;
+    if (error) return this.handleErrors(); // TODO: error handling
+    if (data !== undefined) {
+      this.initialLoading = false;
+      // TODO: set focus
+      this.dataLoading = true;
+      this.quickContentDocumentVersionId = data;
+    }
+    return null;
   }
 
   @wire(getRecord, {
     recordId: "$quickContentDocumentVersionId",
     fields: [ID_FIELD, Content_FIELD]
   })
-  wiredContentDocument(result) {
-    console.log(result);
+  wiredContentDocument({ error, data }) {
+    if (error) return this.handleErrors();
+    this.noteRecord = data;
+    if (this.noteRecord != null) {
+      const encodedContent = this.noteRecord.fields.Content.value;
+      this.htmlContent = window.atob(encodedContent);
+      this.noteRecordContent = this.htmlContent;
+    } else {
+      this.htmlContent = null;
+      this.noteRecordContent = null;
+    }
+    this.dataLoading = false;
+    return null;
   }
 }
