a1b3efde4f29fe12ca696661ef32ccd825202f92
Hugo Dellinger <hugo.dellinger@gmail.com>
2020-05-03 16:32:49 +0200

    Keyboard save

--- force-app/main/default/lwc/quickNote/quickNote.js
+++ force-app/main/default/lwc/quickNote/quickNote.js
@@ -1,33 +1,100 @@
 import { LightningElement, wire, api, track } from "lwc";
-import ID_FIELD from "@salesforce/schema/ContentNote.Id";
-import Content_FIELD from "@salesforce/schema/ContentNote.Content";
+import { updateRecord, createRecord, getRecord } from "lightning/uiRecordApi";
+import { refreshApex } from "@salesforce/apex";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
-import { getRecord } from "lightning/uiRecordApi";
+import CN_ID_FIELD from "@salesforce/schema/ContentNote.Id";
+import CN_CONTENT_FIELD from "@salesforce/schema/ContentNote.Content";
+import CN_TITLE_FIELD from "@salesforce/schema/ContentNote.Title";
+import CN_OBJECT from "@salesforce/schema/ContentNote";
 
 import retrieveQuickContentDocumentIdForRecordId from "@salesforce/apex/QuickNoteController.retrieveQuickContentDocumentIdForRecordId";
+import createNoteLink from "@salesforce/apex/QuickNoteController.createNoteLink";
 
 export default class QuickNote extends LightningElement {
   @api recordId;
   @track quickContentDocumentVersionId;
   noteRecord;
-  noteRecordContent; // To prevent decoding the content everytime we want to check equality with current content, we store the decoded string
-  htmlContent;
 
   // Splitting the loading state for data fecthing in two steps
   // enables us to stop displaying the spinner if no existing note has been found
   // and thus no subsequent data loading happen
   initialLoading = true;
   dataLoading = false;
+  isSaving = false;
   get isLoading() {
-    return this.initialLoading === true || this.dataLoading === true;
+    return (
+      this.initialLoading === true ||
+      this.dataLoading === true ||
+      this.isSaving === true
+    );
   }
 
+  renderedCallback() {
+    document.addEventListener("keydown", this.handleKeyboardSave.bind(this));
+  }
+
+  // # Save status management
   // We store the current content at each change to be able to show the correct state for the save button
+  noteRecordContent; // To prevent decoding the content everytime we want to check equality with current content, we store the decoded string
+  htmlContent;
   get isContentSameAsBefore() {
     return this.noteRecordContent == this.htmlContent; // eslint-disable-line eqeqeq
   }
   handleContentChange({ target }) {
     this.htmlContent = target.value;
+  }
+  get isSaveDisabled() {
+    return this.isContentSameAsBefore === true || this.isSaving === true;
+  }
+
+  async insertNote() {
+    const fields = {
+      [CN_CONTENT_FIELD.fieldApiName]: window.btoa(this.htmlContent),
+      [CN_TITLE_FIELD.fieldApiName]: "QuickNote"
+    };
+    const noteRecord = await createRecord({
+      apiName: CN_OBJECT.objectApiName,
+      fields
+    });
+    this.noteRecord = noteRecord;
+    await createNoteLink({ noteId: noteRecord.id, recordId: this.recordId });
+  }
+
+  async updateNote() {
+    const fields = {
+      [CN_ID_FIELD.fieldApiName]: this.noteRecord.id,
+      [CN_CONTENT_FIELD.fieldApiName]: window.btoa(this.htmlContent),
+      [CN_TITLE_FIELD.fieldApiName]: "QuickNote"
+    };
+    await updateRecord({ fields });
+  }
+
+  // # Save data
+  async handleSave() {
+    try {
+      if (this.isContentSameAsBefore || this.isSaving) return; // don't save if the content is the same as before
+      this.isSaving = true;
+      if (this.noteRecord == null) {
+        await this.insertNote();
+        refreshApex(this.quickContentDocumentVersionId);
+      } else {
+        await this.updateNote();
+        refreshApex(this.noteRecord);
+      }
+    } catch (error) {
+      this.handleErrors({ error });
+    } finally {
+      this.isSaving = false;
+    }
+  }
+  async handleKeyboardSave(event) {
+    if (
+      (event.ctrlKey === true || event.metaKey === true) &&
+      event.keyCode === 83
+    ) {
+      await this.handleSave();
+    }
   }
 
   handleErrors({ error }) {
@@ -37,18 +104,20 @@ export default class QuickNote extends LightningElement {
   @wire(retrieveQuickContentDocumentIdForRecordId, { recordId: "$recordId" })
   wiredContentDocumentId({ error, data }) {
     if (error) return this.handleErrors(); // TODO: error handling
+
     if (data !== undefined) {
       this.initialLoading = false;
-      // TODO: set focus
-      this.dataLoading = true;
-      this.quickContentDocumentVersionId = data;
+      if (data !== null) {
+        this.dataLoading = true;
+        this.quickContentDocumentVersionId = data;
+      }
     }
     return null;
   }
 
   @wire(getRecord, {
     recordId: "$quickContentDocumentVersionId",
-    fields: [ID_FIELD, Content_FIELD]
+    fields: [CN_ID_FIELD, CN_CONTENT_FIELD]
   })
   wiredContentDocument({ error, data }) {
     if (error) return this.handleErrors();
